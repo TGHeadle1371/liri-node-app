579576dd0da27b1325a34e63a98608690909df3a
TGHeadle1371 <tgheadle@gmail.com>
2019-02-03 09:23:31 -0700

    Axios OMDB Added

--- liri.js
+++ liri.js
@@ -9,8 +9,30 @@
 // Node - Spotify - API
 
 // Axios
-
-
+var axios = require("axios");
+movieName = process.argv[2];
+var nodeArgs = process.argv;
+var movieName = "";
+for (var i = 2; i < nodeArgs.length; i++) {
+    if (i > 2 && i < nodeArgs.length) {
+        movieName = movieName + "_" + nodeArgs[i];
+    } else {
+        movieName += nodeArgs[i];
+    }
+}
+axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
+    function (response) {
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.Ratings);
+        console.log(response.data.Genre);
+        console.log("This movie was released in:" + response.data.Released);
+    }
+);
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
 // You 'll use Axios to grab data from the OMDB API and the Bands In Town API
 
 
