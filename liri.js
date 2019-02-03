19eb11e49cbb982c386dda10ac3b663891060dd5
TGHeadle1371 <tgheadle@gmail.com>
2019-02-03 09:16:19 -0700

    Initial files created

---
+++ liri.js
@@ -0,0 +1,24 @@
+// LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+// Make a new GitHub repository called liri - node - app and clone it to your computer.
+// To retrieve the data that will power this app, you 'll need to send requests using the axios package to the Bands in Town, Spotify and OMDB APIs. You'
+// ll find these Node packages crucial
+// for your assignment.
+
+
+
+// Node - Spotify - API
+
+// Axios
+
+
+// You 'll use Axios to grab data from the OMDB API and the Bands In Town API
+
+
+
+// Moment
+// DotEnv
+
+var keys = require("keys.js");
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
