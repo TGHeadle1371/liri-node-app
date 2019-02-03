9682a3a5f59631ba4b7bafe0d1da2a30c30689bb
TGHeadle1371 <tgheadle@gmail.com>
2019-02-03 09:40:40 -0700

    spotify API added

--- liri.js
+++ liri.js
@@ -3,12 +3,43 @@
 // To retrieve the data that will power this app, you 'll need to send requests using the axios package to the Bands in Town, Spotify and OMDB APIs. You'
 // ll find these Node packages crucial
 // for your assignment.
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+// Grab Keys.js and store as keys var
+// require dotenv
+var keys = require("keys.js");
+require("dotenv").config();
 
 
-
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 // Node - Spotify - API
-
-// Axios
+// Access keys information with spotify
+var spotify = new Spotify(keys.spotify);
+// var Spotify = require('node-spotify-api');
+songTitle = process.argv[2];
+var nodeAargs = process.argv;
+var songTitle = "";
+for (var i = 2; i < nodeAargs.length; i++) {
+    if (i > 2 && i < nodeAargs.length) {
+        songTitle = songTitle + "_" + nodeAargs[i];
+    } else {
+        songTitle += nodeAargs[i];
+    }
+}
+spotify.search({
+        type: 'track',
+        query: songTitle
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+// Axios OMDB
 var axios = require("axios");
 movieName = process.argv[2];
 var nodeArgs = process.argv;
@@ -39,8 +70,3 @@ console.log(queryUrl);
 
 // Moment
 // DotEnv
-
-var keys = require("keys.js");
-require("dotenv").config();
-
-var spotify = new Spotify(keys.spotify);
